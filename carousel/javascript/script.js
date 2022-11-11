f3a66e6a2b903c9b01c90ccfe4b8510fc9b29a0f
Nelson Chimdiadi <64903808+Youngman-d-coder@users.noreply.github.com>
2022-11-11 05:03:06 +0100

    Add files via upload

---
+++ carousel/javascript/script.js
@@ -0,0 +1,17 @@
+var currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("imageSlides");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
